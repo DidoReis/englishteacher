90cd5f0a5bc13d8eda71756b5c917c5d4c23850e
DidoReis <djdidoreis@gmail.com>
2023-06-06 06:52:34 -0300

    1 commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# English Teacher - Spoken English Improvement
+
+This project aims to help improve spoken English skills through conversation practice with an AI English teacher. The AI teacher will prompt you with questions in Portuguese, and you will respond in English. The AI teacher will provide feedback, correct errors, and ask follow-up questions to enhance your spoken English.
+
+## Installation
+
+1. Clone the repository:
+```shell
+git clone https://github.com/your-username/english-teacher.git
+
+
+2. Create a virtual environment (optional but recommended):
+cd english-teacher
+python3 -m venv env
+
+
+3. Activate the virtual environment:
+source env/bin/activate  # for Linux/macOS
+env\Scripts\activate  # for Windows
+
+
+4. Install the required dependencies:
+pip install -r requirements.txt
+
+
+
+## Usage
+Run the script:
+python main.py
+
+The AI teacher will ask you a random question in English.
+Speak your response in English into the microphone.
+The AI teacher will process your response, provide feedback, correct errors, and ask follow-up questions.
+Continue the conversation, practicing your spoken English skills with the AI teacher.
+
+License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+Feel free to customize the content and structure of the README file to better fit your project's requirements.
+
+Happy coding!
---
+++ main.py
@@ -0,0 +1,85 @@
+# flake8: noqa
+import speech_recognition as sr
+import pyttsx3
+import openai
+import random
+
+openai.api_key = 'Your Api Key'
+
+
+def recognize_speech():
+    r = sr.Recognizer()
+    
+    initial_question = random.choice([
+        "What is your favorite hobby?",
+        "Tell me about your favorite movie.",
+        "What do you enjoy doing in your free time?"
+    ])
+
+    with sr.Microphone() as source:
+        print(initial_question)
+        audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio, language="pt-BR")
+        print("You answered:", text)
+
+        prompt = create_prompt(initial_question, text)
+
+        response = generate_response(prompt)
+        answer = get_model_answer(response)
+
+        print("Model's response:", answer)
+
+        speak_response(answer)
+
+    except sr.UnknownValueError:
+        print("Unable to recognize audio.")
+
+
+def create_prompt(question, answer):
+    prompt = (
+        "Act as a teacher and language improver for spoken English. "
+        "I will speak to you in Portuguese, and you will respond in English "
+        "to practice my spoken English. Translate your responses into Portuguese "
+        "only if I ask.\n\n"
+        "I want you to:\n"
+        "- Keep your response organized, limiting it to 100 words.\n"
+        "- Point out and correct any conjugation, spelling, grammar, and other "
+        "errors I make; this is most important.\n"
+        "- Ask me a question in your response.\n\n"
+        "Your questions should always be in English, and you should use Portuguese "
+        "only to explain my errors.\n\n"
+        "Now let's start practicing. Answer the following question in English:\n\n"
+        f"{question}\n\nYour response: {answer}"
+    )
+    return prompt
+
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100
+    )
+    return response
+
+
+def get_model_answer(response):
+    answer = response.choices[0].text.strip()
+    return answer
+
+
+def speak_response(answer):
+    engine = pyttsx3.init()
+    engine.say(answer)
+    engine.runAndWait()
+
+
+recognize_speech()
+
+
+
+
+
+
